8f55d9844064b0ac15e949ae4ca44056966e8134
AsuraDiso <64610723+AsuraDiso@users.noreply.github.com>
2022-03-14 19:49:10 +0200

    Initial commit

---
+++ modinfo.lua
@@ -0,0 +1,17 @@
+
+name = "A New Reign Mod"
+description = "A New Reign Mod"
+author = "Leonidas IV, Asura"
+version = "1.0"
+
+forumthread = "nil"
+
+api_version = 6
+
+dont_starve_compatible = false
+reign_of_giants_compatible = true
+shipwrecked_compatible = false
+hamlet_compatible = false
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
