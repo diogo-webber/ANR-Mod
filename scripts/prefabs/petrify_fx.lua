e578fa406b94a73ee03a718058d326d1582e5fde
AsuraDiso <64610723+AsuraDiso@users.noreply.github.com>
2022-03-16 20:34:18 +0200

    Fixed petrify_fx

--- scripts/prefabs/petrify_fx.lua
+++ scripts/prefabs/petrify_fx.lua
@@ -9,61 +9,44 @@ local treeassets =
 }
 
 local function SerializeDarken(inst, r, g, b)--, a)
-    inst._darken:set(math.clamp(math.floor(14 * (r + g + b) / 3 - 6.5), 0, 7))
+    inst._darken = math.clamp(math.floor(14 * (r + g + b) / 3 - 6.5), 0, 7)
 end
 
 local function DeserializeDarken(inst)
-    local val = inst._darken:value() / 14 + .5
+    local val = inst._darken / 14 + .5
     return val, val, val, 1
 end
 
-local function PlayFX(proxy, assetname, animname, soundname)
-    local inst = CreateEntity()
-
-    inst:AddTag("FX")
-    --[[Non-networked entity]]
-    inst.entity:SetCanSleep(false)
-    inst.persists = false
-
-    inst.entity:AddTransform()
-    inst.entity:AddAnimState()
-    inst.entity:AddSoundEmitter()
-
-    --inst.Transform:SetFromProxy(proxy.GUID)
-
-    inst.AnimState:SetBank(assetname)
-    inst.AnimState:SetBuild(assetname)
-    inst.AnimState:PlayAnimation(animname)
-    --inst.AnimState:SetMultColour(DeserializeDarken(proxy))
-    inst.AnimState:SetFinalOffset(1)
-
-    inst.SoundEmitter:PlaySound("dontstarve/common/together/petrified/"..soundname)
-
-    inst:ListenForEvent("animover", inst.Remove)
-end
-
 local function SetDarkened(inst, val)
-    inst._darken:set(math.clamp(math.floor((2 * val - 1) * 7 + .5), 0, 7))
+    inst._darken = math.clamp(math.floor((2 * val - 1) * 7 + .5), 0, 7)
 end
 
 local function makefx(assetname, animname, soundname)
     return function()
         local inst = CreateEntity()
 
-        inst.entity:AddTransform()
-
         inst:AddTag("FX")
-
-        inst:DoTaskInTime(0, PlayFX, assetname, animname, soundname)
-
-        --inst._darken = net_tinybyte(inst.GUID, "petrifyfx._darken")
-        --inst._darken:set(7)
-
-
-        --inst.InheritColour = SerializeDarken
-
+        --[[Non-networked entity]]
+        inst.entity:SetCanSleep(false)
         inst.persists = false
-        inst:DoTaskInTime(1, inst.Remove)
+    
+        inst.entity:AddTransform()
+        inst.entity:AddAnimState()
+        inst.entity:AddSoundEmitter()
+
+        inst._darken = 7
+   
+        inst.AnimState:SetBank(assetname)
+        inst.AnimState:SetBuild(assetname)
+        inst.AnimState:PlayAnimation(animname)
+        inst.AnimState:SetMultColour(DeserializeDarken(inst))
+        inst.AnimState:SetFinalOffset(1)
+    
+        inst.SoundEmitter:PlaySound("dontstarve/common/together/petrified/"..soundname)
+    
+        inst:ListenForEvent("animover", inst.Remove)
+
+        inst.InheritColour = SerializeDarken
 
         return inst
     end
